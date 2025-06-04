d979b5cdea1122c1d05a36ca736671de2930a703
kalocesa <kalocesa95@gmail.com>
2025-06-03 19:08:28 -0600

    add: cors en app.js

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,21 @@ const auth = require("./middlewares/auth");
 const errorHandler = require("./middlewares/errorHandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const cors = require("cors");
+let corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg =
+        "The CORS policy for this site does not " +
+        "allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 require("dotenv").config();
 // Conexión a MongoDB
 mongoose
@@ -21,8 +36,12 @@ mongoose
 // Utilizar rutas
 app.use(express.json());
 
-app.use(cors());
-app.options("*", cors());
+app.use(
+  cors({
+    origin: corsOptions,
+    credentials: true,
+  })
+);
 // Importar rutas
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
